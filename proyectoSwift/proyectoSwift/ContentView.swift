d7bce8dad37154afd6c935a24ffb9b18d9e4294d
PabloEnguix09 <pabloenguixllopis@gmail.com>
2024-07-16 13:37:17 +0200

    Project created

---
+++ proyectoSwift/proyectoSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  proyectoSwift
+//
+//  Created by Pablo Enguix Llopis on 16/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ proyectoSwift/proyectoSwift/proyectoSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  proyectoSwiftApp.swift
+//  proyectoSwift
+//
+//  Created by Pablo Enguix Llopis on 16/7/24.
+//
+
+import SwiftUI
+
+@main
+struct proyectoSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
